12fed901d8764ffc185d19ef63d991bc652c45b9
VitPa <vitopiazzolla25@gmail.com>
2025-06-08 08:40:51 +0200

    Sistemato constraints iniziali

--- InitialCondition.c
+++ InitialCondition.c
@@ -3,30 +3,46 @@
 #include <math.h>
 #include "Atmosphere.h"
 
-void checkVelAlt(double *V, double h, double Mdg, double temp_h) {
+void checkVelAlt(double *V, double *h, double *gamma) {
     if (*V < 30){
         printf("[~]WARNING: La velocità inserita è inferiore di quella di stallo.\tInserita velocità minima: 30 m/s\n");
         *V = 30;
     }
-    if (*V > 75){
+    else if (*V > 75){
         printf("[~]WARNING: La velocità inserita è maggiore di quella massima.\tInserita velocità massima: 75 m/s\n");
         *V = 75;
     }
-    if (*V/(sqrt(1.4 * 287.05 * temp_h)) > Mdg){
-        printf("[!]ERROR: La velocità è maggiore del match di drag rise.\nSIMULAZIONE TERMINATA!");
-        system("PAUSE");
-        exit(0);
+    if (*h<0){
+        printf("[~]WARNING: I valori inseriti corrispondono ad una quota minore di zero.\nInserita quota minima: 0 m\n");
+        *h = 0;
+    }
+    else if(*h>4116){
+        printf("[~]WARNING: I valori inseriti corrispondono ad una quota maggiore di quella di tangenza.\nInserita quota massima: 4116 m\n");
+        *h = 4116;
+    }
+    if (*gamma<-5){
+        printf("[~]WARNING: L'angolo di rampa è inferiore di quello minimo consentito.\tInserito angolo di rampa minimo: -5 deg\n");
+        *gamma = -5;
+    }
+    if (*gamma>10){
+        printf("[~]WARNING: L'angolo di rampa è maggiore di quello massimo consentito.\tInserito angolo di rampa massimo: 10 deg\n");
+        *gamma = 10;
+    }
+}
+
+void physicalCheck(double V, double h, double Mdg, double vsuono_h) {
+    static int f1 = 0, f2 = 0, f3 = 0;
+    if (V/(sqrt(vsuono_h)) > Mdg){
+        if(++f1>1) { printf("[!]ERROR: La velocità è maggiore del match di drag rise.\nSIMULAZIONE TERMINATA!");system("PAUSE"); exit(0);}
     }
     if(h<0){
-        printf("[!]ERROR: I valori inseriti corrispondono ad una quota minore di zero.\nSIMULAZIONE TERMINATA!");
-        system("PAUSE");
-        exit(0);
+        if(++f2>1) {printf("[!]ERROR: Il velivolo ha raggiunto quota zero.\nSIMULAZIONE TERMINATA!"); system("PAUSE"); exit(0);}
     }
+    else if(h==4116) printf("[~]WARNING: Il velivolo ha raggiunto la quota di tangenza.\n");
     else if(h>4116){
-        printf("[!]ERROR: I valori inseriti corrispondono ad una quota maggiore di quella di tangenza.\nSIMULAZIONE TERMINATA!\n");
-        system("PAUSE");
-        exit(0);
+        if(++f3>1) {printf("[!]ERROR: Il velivolo ha raggiunto la quota di tangenza.\nSIMULAZIONE TERMINATA!\n"); system("PAUSE"); exit(0);}
     }
+    f1 = 0, f2 = 0, f3 = 0;
 }
 
 void loadCI(double *CI) {
@@ -38,7 +54,6 @@ void loadCI(double *CI) {
     
     printf("Inserire la velocità inziale [m/s]: ");
     do{
-        
         fgets(input, 100, stdin);
         if(input[0]=='\n') {
             CI[0] = 52; 
@@ -50,7 +65,6 @@ void loadCI(double *CI) {
     }while(1);
     printf("Inserire l'altitudine inziale [m]: ");
     do{
-        
         fgets(input, 100, stdin);
         if(input[0]=='\n') {
             CI[1] = 1000; 
@@ -62,7 +76,6 @@ void loadCI(double *CI) {
     }while(1);
     printf("Inserire l'angolo di attacco inziale [deg]: ");
     do{
-        
         fgets(input, 100, stdin);
         if(input[0]=='\n') {
             CI[2] = 0; 
--- InitialCondition.h
+++ InitialCondition.h
@@ -1,7 +1,9 @@
 #ifndef INITIALCONDITION_H
 #define INITIALCONDITION_H
 
-void checkVelAlt(double *V, double h, double Mdg, double temp_h);
+void checkVelAlt(double *V, double *h, double *gamma);
+
+void physicalCheck(double V, double h, double Mdg, double vsuono_h);
 
 void loadCI(double *CI);
 
